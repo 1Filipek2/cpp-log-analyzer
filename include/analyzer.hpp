1a22bf1904c1eeec57b29548744141e0398e7cd4
1Filipek2 <rybakovfilip12@gmail.com>
2026-03-21 13:02:55 +0100

    implement level statistics

--- include/analyzer.hpp
+++ include/analyzer.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+
+#include "log_entry.hpp"
+
+std::map<std::string, int> countLevels(const std::vector<LogEntry>& entries);
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
 
+#include "analyzer.hpp"
 #include "parser.hpp"
 
 int main () {
     std::vector<LogEntry> entries = parseLogFile("../sample/sample.log");
 
-    std::cout << "Loaded valid entries:" << entries.size() << std::endl;
+    std::cout << "Loaded valid entries: " << entries.size() << std::endl;
 
-    for (const auto& entry : entries) {
-        std::cout << "Date: " << entry.date << std::endl;
-        std::cout << "Time: " << entry.time << std::endl;
-        std::cout << "Level: " << entry.level << std::endl;
-        std::cout << "Message: " << entry.message << std::endl;
+    std::map<std::string, int> levelCounts = countLevels(entries);
+
+    std::cout << "Level counts: " << std::endl;
+
+    for (const auto& [level, count] : levelCounts) {
+        std::cout << level << ": " << count << std::endl;
     }
+
     return 0;
 }
